d41df048269fd3e62d28bd915ea246fb9c8850f3
bsing <bsinghal555@gmail.com>
2024-02-29 21:02:36 +0530

    Appium Framework initial commit

---
+++ src/test/java/tests/ExtentReportsDemo.java
@@ -0,0 +1,31 @@
+package tests;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+
+
+public class ExtentReportsDemo {
+
+	ExtentReports extent;
+	 ExtentSparkReporter spark;
+	  
+	  
+	@BeforeSuite
+	public void setupReport()
+	{
+		extent = new ExtentReports();
+       spark = new ExtentSparkReporter("Spark.html");
+       extent.attachReporter(spark);
+	}
+	
+	@AfterSuite
+	public void teardownReport()
+	{
+		extent.flush();
+	}
+	
+}
